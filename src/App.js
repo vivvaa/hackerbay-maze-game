137fb703c357acf764a2262898f9a11470ad6f68
Ogbonna Vitalis <agavitalisogbonna@gmail.com>
2020-03-23 15:04:28 +0100

    Fixing of some error

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ let max_mashroom;
 function Welcome(props) {
 	return (
 		<div>
-			<h2>Mario Maze Game</h2>
+			<h2>React Maze Game</h2>
 			<p>You need to eat all the mashrooms in 64 steps. By Default maze size is 10 x 10, you can change it by reloading or entering the height and width in prompt.</p>
 		</div>
 		)
